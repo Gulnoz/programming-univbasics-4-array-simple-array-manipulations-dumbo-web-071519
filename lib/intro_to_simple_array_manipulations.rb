0707bf19681db0ea7b9993ce712861cc318d7a57
fortuitous-counter-8979 <gmcharmy@gmail.com>
2019-06-20 01:04:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,9 +2,11 @@ def using_push(array, string)
 array.push(string)
 end
 def using_unshift(array, srting)
+array.unshift(string)
 end
 
-def using_pop(array, srting)
+def using_pop(array)
+array.pop
 end
 
 def pop_with_args(array, srting)
